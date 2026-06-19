8c000b68bb94416c9f332c74d2bdc98756974ed7
am225723 <puertaale@gmail.com>
2026-06-19 13:17:04 -0400

    Add scroll performance controller to sign-in overlay

--- src/components/PrototypeIFSLoginOverlay.jsx
+++ src/components/PrototypeIFSLoginOverlay.jsx
@@ -1,7 +1,32 @@
+import { useEffect } from 'react';
 import FinalIFSLoginPage from './FinalIFSLoginPage.jsx';
 
 export default function PrototypeIFSLoginOverlay() {
   const isSignInPath = window.location.pathname.startsWith('/sign-in');
+
+  useEffect(() => {
+    if (!isSignInPath) return undefined;
+
+    let scrollTimer;
+    const root = document.documentElement;
+    const setScrolling = () => {
+      root.classList.add('ifs-login-is-scrolling');
+      window.clearTimeout(scrollTimer);
+      scrollTimer = window.setTimeout(() => {
+        root.classList.remove('ifs-login-is-scrolling');
+      }, 180);
+    };
+
+    window.addEventListener('scroll', setScrolling, { passive: true });
+    window.addEventListener('touchmove', setScrolling, { passive: true });
+
+    return () => {
+      window.clearTimeout(scrollTimer);
+      root.classList.remove('ifs-login-is-scrolling');
+      window.removeEventListener('scroll', setScrolling);
+      window.removeEventListener('touchmove', setScrolling);
+    };
+  }, [isSignInPath]);
 
   if (!isSignInPath) {
     return null;
