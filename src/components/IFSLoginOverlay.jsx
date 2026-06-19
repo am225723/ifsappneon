b7263149f51595ddd7964e434de1d8db45061967
am225723 <puertaale@gmail.com>
2026-06-19 00:30:42 -0400

    Import IFS login overlay styles

--- src/components/IFSLoginOverlay.jsx
+++ src/components/IFSLoginOverlay.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useAuth } from '@clerk/clerk-react';
 import IFSLoginPage from './IFSLoginPage.jsx';
+import './IFSLoginOverlay.css';
 
 function getCurrentPath() {
   return window.location.pathname;
